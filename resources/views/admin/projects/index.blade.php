562d53e7721044de0cb49fad97b7814dc26c8fb4
nembo93 <francesco.c193@libero.it>
2023-01-26 18:53:03 +0100

    Implementa delete

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -25,12 +25,50 @@
                 <td>
                     <a href="{{ route('admin.projects.show', $project) }}" class="btn btn-success"><i class="fa-solid fa-eye"></i></a>
                     <a href="{{ route('admin.projects.edit', $project) }}" class="btn btn-warning"><i class="fa-solid fa-pen"></i></a>
-                <!-- Button trigger modal -->
-                {{-- <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#modal-{{$project->id}}">
-                    <i class="fa-solid fa-trash"></i>
-                </button> --}}
+                    <!-- Button trigger modal -->
+                    <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#modal-{{$project->id}}">
+                        <i class="fa-solid fa-trash"></i>
+                    </button>
                 </td>
             </tr>
+            <!-- Modal -->
+            <div class="modal fade" id="modal-{{$project->id}}" tabindex="-1">
+                <div class="modal-dialog">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h1 class="modal-title fs-5" id="exampleModalLabel">Stai per eliminare il progetto</h1>
+                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                      Sei sicuro di eliminare il progetto "{{$project->title}}"?
+                    </div>
+                    <div class="modal-footer">
+                      <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">No</button>
+                      <form action="{{ route('admin.projects.destroy', $project) }}" method="POST" class="d-inline-block">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-primary">Si</button>
+                      </form>
+                    </div>
+                  </div>
+                </div>
+              </div>
             @endforeach
+        </tbody>    
     </table>
+
+    @if( session('message') )
+        <div class="toast-container position-fixed bottom-0 end-0 p-3">
+            <div id="liveToast" class="toast show" role="alert" aria-live="assertive" aria-atomic="true">
+            <div class="toast-header">
+                <strong class="me-auto">Notifica</strong>
+                <small>{{ \Carbon\Carbon::now()->diffForHumans() }}</small>
+                <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+            </div>
+            <div class="toast-body">
+                {{session('message')}}
+            </div>
+            </div>
+        </div>
+    @endif
 @endsection
