d21802bdb3bb1fdb87bdb4f4818f212fae4f27ce
nembo93 <francesco.c193@libero.it>
2023-01-26 16:08:01 +0100

    Lancia migration e seeder con dati faker

--- database/migrations/2023_01_26_132428_create_projects_table.php
+++ database/migrations/2023_01_26_132428_create_projects_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 150)->unique();
+            $table->text('description');
+            $table->string('customer', 50);
+            $table->string('slug');
             $table->timestamps();
         });
     }
